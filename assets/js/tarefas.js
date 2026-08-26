f957635221974dc28251e8fada56f106be2dced5
ericovinicosta <elcosta@gmail.com>
2026-08-25 23:46:39 -0300

    4C - Exibindo em tabelas

--- assets/js/tarefas.js
+++ assets/js/tarefas.js
@@ -15,20 +15,38 @@ submitButton.addEventListener('click', (event) => {
     }
 });
 
+const buscarPrioridade = (prioridade) => {
+    switch (prioridade) {
+        case '1':
+            return 'Alta';
+        case '2':
+            return 'Média';
+        case '3':
+            return 'Baixa';
+    }
+}
+
+const formatarData = (data) => {
+    const partesData = data.split('-');
+    return `${partesData[2]}/${partesData[1]}/${partesData[0]}`;
+}
+
 const mostrarTarefas = () => {
-    const tarefasContainer = document.querySelector('#task-list');
+    const tarefasContainer = document.querySelector('#task-list tbody');
     tarefasContainer.innerHTML = '';
 
-    tarefas.forEach(tarefa => {
-        const tarefaElement = document.createElement('li');
+    tarefas.forEach(tarefa => { 
+        const tarefaElement = document.createElement('tr');
         tarefaElement.classList.add('task-item');
         tarefaElement.innerHTML = `
-            <h3>${tarefa.titulo}</h3>
-            <p>Prioridade: ${tarefa.prioridade}</p>
-            <p>Prazo: ${tarefa.prazo}</p>
-            <p>Status: ${tarefa.status ? 'Concluída' : 'Pendente'}</p>
-            <button class="edit-btn" data-codigo="${tarefa.codigo}">Editar</button>
-            <button class="delete-btn" data-codigo="${tarefa.codigo}">Excluir</button>
+            <td>${tarefa.titulo}</td>
+            <td>${buscarPrioridade(tarefa.prioridade)}</td>
+            <td>${formatarData(tarefa.prazo)}</td>
+            <td>${tarefa.status ? 'Concluída' : 'Pendente'}</td>
+            <td>
+                <button class="edit-btn" data-codigo="${tarefa.codigo}">Editar</button>
+                <button class="delete-btn" data-codigo="${tarefa.codigo}">Excluir</button>
+            </td>
         `;
         tarefasContainer.appendChild(tarefaElement);
     });
@@ -47,7 +65,7 @@ const mostrarTarefas = () => {
                 event.preventDefault();
                 try {
                     atualizarTarefa(codigo, document.querySelector('#task-input').value, document.querySelector('#task-priority').value, document.querySelector('#task-deadline').value, tarefa.status);
-                    mostrarTarefas();
+                    mostrarTarefas();''
                     submitButton.textContent = 'Adicionar';
                 } catch (error) {
                     console.error('Error:', error);
